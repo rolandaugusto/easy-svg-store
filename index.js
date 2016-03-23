a69b95f98953282ce69914e13cb30a8576dfa200
Roland Castillo <rolandaugusto@gmail.com>
2016-03-24 00:53:15 +0100

    running in parallel

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 
 var fs = require('fs');
 var _path = require('path');
-
+var readMultipleFiles = require('read-multiple-files');
 
 module.exports = function (path, options) {
 
     path = path || '';
     options = options || {};
 
-
     fs.readdir(path, function(err, files) {
+        var filesPath = [];
+
         if (err) {
             console.error(err);
             return;
         }
-        files.forEach(function(filename) {
-            console.log(files);
-            fs.readFile(_path.join(path, filename), 'utf-8', function (err, data) {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                console.log(data);
-            })
+
+        for (var i=0; i < files.length; i++) {
+            filesPath[i] = _path.join(path, files[i]);
+        }
+
+        readMultipleFiles(filesPath, 'utf-8', function (err, contents) {
+
+            fs.writeFile(_path.join(path, 'svgstore.svg'), contents)
+
         });
     });
 
