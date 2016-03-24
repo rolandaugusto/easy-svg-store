3634f684401a0f8809afa5c7ff05f1a413c9d140
Roland Castillo <roland.castillo@zalando.de>
2016-03-24 14:01:04 +0100

    first version

--- index.js
+++ index.js
@@ -15,35 +15,39 @@ module.exports = function (path, options) {
     options.svgSpriteName = options.svgSpriteName || 'svgsprite';
 
     fs.readdir(path, function (err, files) {
-        var filesPath = [];
+        var filesPath = [],
+            processedFiles = [],
+            initialFiles = files.length;
         if (err) {
             console.error(err);
             return;
         }
 
         files = files.filter(function (value) {
-            return value !== options.svgSpriteName + '.svg' || _path.extname(value) === 'png';
+            return value !== options.svgSpriteName + '.svg' && _path.extname(value) === '.svg';
         });
 
         for (var i = 0; i < files.length; i++) {
-            // if (_path.extname(files[i]) === '.svg') {
                 filesPath[i] = _path.join(path, files[i]);
-            // } else {
-            //     console.log('skipping file with extension: ', _path.extname(files[i]));
-            // }
         }
 
-        console.log(`Total files in folder: ${files.length}, SVGs found: ${filesPath.length}`);
+        console.log(`Total files in folder: ${initialFiles}, SVGs found: ${filesPath.length}`);
 
         readMultipleFiles(filesPath, 'utf-8', function (err, contents) {
 
             contents.map(function (item, index) {
                 var symbolId = files[index].split('.')[0].toLowerCase();
+                item = item.replace(/xmlns="http:\/\/www.w3.org\/2000\/svg"/g, '');
                 item = item.replace(/<svg /g, '<symbol id="' + symbolId + '" ');
                 item = item.replace(/<\/svg>/g, '</symbol>');
+                processedFiles += item;
                 console.log(item);
             });
-            fs.writeFile(_path.join(path, options.svgSpriteName + '.svg'), contents);
+
+            var svgTopBefore = '<svg xmlns="http://www.w3.org/2000/svg">';
+            var svgTopAfter = '</svg>';
+
+            fs.writeFile(_path.join(path, options.svgSpriteName + '.svg'), svgTopBefore + processedFiles + svgTopAfter);
         });
     });
 };
