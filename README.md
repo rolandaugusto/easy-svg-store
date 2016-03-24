205cddab596d7d79398806c5f52d7af063a3d64c
Roland Castillo <roland.castillo@zalando.de>
2016-03-24 13:47:44 +0100

    read files gnerate sprite, still in progress

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # easy-svg-store
 
 Currently under Development...
+
+Usage
+======
+
+var path = require('path');
+var esvgs = require('easy-svg-store');
+
+esvgs(path.join(__dirname, '..', 'svgs'),
+      {
+        svgSpriteName: 'svgstore'
+      }
+  );
--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-
+/*
+* Easy SVG store
+*
+* No Fuzz svg sprite
+*
+* */
 var fs = require('fs');
 var _path = require('path');
 var readMultipleFiles = require('read-multiple-files');
 
 module.exports = function (path, options) {
-
     path = path || '';
     options = options || {};
 
-    fs.readdir(path, function(err, files) {
-        var filesPath = [];
+    options.svgSpriteName = options.svgSpriteName || 'svgsprite';
 
+    fs.readdir(path, function (err, files) {
+        var filesPath = [];
         if (err) {
             console.error(err);
             return;
         }
 
-        for (var i=0; i < files.length; i++) {
-            filesPath[i] = _path.join(path, files[i]);
+        files = files.filter(function (value) {
+            return value !== options.svgSpriteName + '.svg' || _path.extname(value) === 'png';
+        });
+
+        for (var i = 0; i < files.length; i++) {
+            // if (_path.extname(files[i]) === '.svg') {
+                filesPath[i] = _path.join(path, files[i]);
+            // } else {
+            //     console.log('skipping file with extension: ', _path.extname(files[i]));
+            // }
         }
 
+        console.log(`Total files in folder: ${files.length}, SVGs found: ${filesPath.length}`);
+
         readMultipleFiles(filesPath, 'utf-8', function (err, contents) {
-
-            fs.writeFile(_path.join(path, 'svgstore.svg'), contents)
-
+""
+            contents.map(function (item, index) {
+                var symbolId = files[index].split('.')[0].toLowerCase();
+                item = item.replace(/<svg /g, '<symbol id="' + symbolId + '" ');
+                item = item.replace(/<\/svg>/g, '</symbol>');
+                console.log(item);
+            });
+            fs.writeFile(_path.join(path, options.svgSpriteName + '.svg'), contents);
         });
     });
-
-    return true;
-
 };
